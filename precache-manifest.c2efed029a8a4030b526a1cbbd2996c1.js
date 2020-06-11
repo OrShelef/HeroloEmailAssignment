4f81f95e46112adc8be41afb14692990c5b9016f
ori shelef <ori9262317@gmail.com>
2020-06-11 21:14:33 +0300

    Updates

---
+++ precache-manifest.c2efed029a8a4030b526a1cbbd2996c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31b2b4731d6314538b544276c27a685",
+    "url": "/HeroloEmailAssignment/index.html"
+  },
+  {
+    "revision": "bf9d8414cbdd33cfb1ab",
+    "url": "/HeroloEmailAssignment/static/css/main.39bfaff9.chunk.css"
+  },
+  {
+    "revision": "d237493a7352fbbbf54c",
+    "url": "/HeroloEmailAssignment/static/js/2.1e50f897.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/HeroloEmailAssignment/static/js/2.1e50f897.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf9d8414cbdd33cfb1ab",
+    "url": "/HeroloEmailAssignment/static/js/main.3103b6c9.chunk.js"
+  },
+  {
+    "revision": "cddbdf4e9701232db39b",
+    "url": "/HeroloEmailAssignment/static/js/runtime-main.4f77a0f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/HeroloEmailAssignment/static/media/logo.5d5d9eef.svg"
+  }
+]);
